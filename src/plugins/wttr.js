d14455471360004d89755e6364843cd4892cd6fe
Joe Woods <code@joewoods.dev>
2021-04-25 17:46:41 -0400

    Show weather for multiple locations

--- src/plugins/wttr.js
+++ src/plugins/wttr.js
@@ -1,4 +1,17 @@
 const fetch = require("node-fetch");
+const { weatherLocations } = require("../../settings");
+
+const getWeather = function resolveLocationToWeatherString(location) {
+  return new Promise((resolve) => {
+    fetch(`https://wttr.in/${location}?format=j1`)
+      .then((res) => res.json())
+      .then((res) =>
+        resolve(
+          `${location}: ${res.current_condition[0].weatherDesc[0].value}, ${res.current_condition[0].temp_C}°C (${res.current_condition[0].temp_F}°F)`
+        )
+      );
+  });
+};
 
 module.exports = {
   command: "weather",
@@ -7,13 +20,20 @@ module.exports = {
   on: "message",
 
   fn({ text: location }) {
-    // TODO: check weather for all known locations if text is false
-    fetch(`https://wttr.in/${location}?format=j1`)
-      .then((res) => res.json())
-      .then((res) =>
+    if (!location && weatherLocations) {
+      Promise.all(
+        weatherLocations.map((loc) => getWeather(loc))
+      ).then((responses) =>
         this.reply(
-          `${location}: ${res.current_condition[0].weatherDesc[0].value}, ${res.current_condition[0].temp_C}°C (${res.current_condition[0].temp_F}°F)`
+          responses.reduce(
+            (prev, current, index, array) =>
+              prev + current + (index === array.length - 1 ? "" : "\n"),
+            ""
+          )
         )
       );
+    } else {
+      getWeather(location).then((res) => this.reply(res));
+    }
   },
 };
