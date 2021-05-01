737352624b093a5da6454d83b7106e8825518c63
Joe Woods <code@joewoods.dev>
2021-05-01 12:35:17 -0400

    Restrict echoChamber to the same channel

--- src/plugins/echoChamber.js
+++ src/plugins/echoChamber.js
@@ -1,12 +1,15 @@
 let previousMessage = "@%#%235235@#%@#%gkldsjfgkdlsjg@#%@#%1";
+let previousChannel = "dfsjkldsfkjldsfjkl!@%$!#%!#%";
 
 module.exports = {
   on: "message",
 
   fn({ channel, text }) {
-    if (text === previousMessage) {
+    const channelName = channel.name;
+    if (text === previousMessage && channelName === previousChannel) {
       channel.send(text);
     }
     previousMessage = text;
+    previousChannel = channelName;
   },
 };
